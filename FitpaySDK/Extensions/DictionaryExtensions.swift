4df5e2b18f134e50f7ed9111669f2c96dd9161c1
walkline <walkline.ua@gmail.com>
2017-05-16 15:40:47 +0300

    Added language support for RTM config. Also added support for creating custom config fields. Now users of SDK can create their custom configs with 2 options: 1. Use RtmConfig.customs; 2. Implement RtmConfigProtocol.

--- FitpaySDK/Extensions/DictionaryExtensions.swift
+++ FitpaySDK/Extensions/DictionaryExtensions.swift
@@ -1,32 +1,24 @@
 
-extension Dictionary
-{
-    var JSONString:String?
-    {
+extension Dictionary {
+    var JSONString: String? {
         return Foundation.JSONSerialization.JSONString(self as AnyObject)
     }
 }
 
-func += <KeyType, ValueType> (left: inout Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>)
-{
-    for (k, v) in right
-    {
+func += <KeyType, ValueType> (left: inout Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>) {
+    for (k, v) in right {
         left.updateValue(v, forKey: k)
     }
 }
 
-
-func + <KeyType, ValueType> (left: Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>) -> [KeyType:ValueType]
-{
-    var dict:[KeyType : ValueType] = [KeyType : ValueType]()
+func + <KeyType, ValueType> (left: Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>) -> [KeyType:ValueType] {
+    var dict: [KeyType : ValueType] = [KeyType : ValueType]()
     
-    for (k, v) in left
-    {
+    for (k, v) in left {
         dict.updateValue(v, forKey: k)
     }
     
-    for (k, v) in right
-    {
+    for (k, v) in right {
         dict.updateValue(v, forKey: k)
     }
     
--- FitpaySDK/Rtm/RtmConfig.swift
+++ FitpaySDK/Rtm/RtmConfig.swift
@@ -1,18 +1,53 @@
 import ObjectMapper
 
-open class RtmConfig: NSObject, Mappable {
+public enum RtmConfigDafaultMappingKey: String {
+    case clientId = "clientId"
+    case redirectUri = "redirectUri"
+    case userEmail = "userEmail"
+    case deviceInfo = "paymentDevice"
+    case hasAccount = "account"
+    case version = "version"
+    case demoMode = "demoMode"
+    case customCSSUrl = "themeOverrideCssUrl"
+    case demoCardGroup = "demoCardGroup"
+    case accessToken = "accessToken"
+    case language = "language"
+    case baseLanguageUrl = "baseLangUrl"
+}
+
+@objc public protocol RtmConfigProtocol {
+    var clientId: String? { get }
+    var redirectUri: String? { get }
+    var deviceInfo: DeviceInfo? { get set }
+    var accessToken: String? { get set }
+    var hasAccount: Bool { get }
+
+    func update(value: Any, forKey: String)
+
+    func jsonDict() -> [String: Any]
+}
+
+open class RtmConfig: NSObject, Mappable, RtmConfigProtocol {
     open var clientId: String?
     open var redirectUri: String?
     open var userEmail: String?
     open var deviceInfo: DeviceInfo?
-    open var hasAccount: Bool?
+    open var hasAccount: Bool = false
     open var version: String?
     open var demoMode: Bool?
     open var customCSSUrl: String?
     open var demoCardGroup: String?
     open var accessToken: String?
+    open var language: String?
+    open var baseLanguageUrl: String?
     
-    public init(clientId:String = FitpaySDKConfiguration.defaultConfiguration.clientId, redirectUri:String = FitpaySDKConfiguration.defaultConfiguration.redirectUri, userEmail:String?, deviceInfo:DeviceInfo?, hasAccount:Bool = false) {
+    open var customs: [String:Any]?
+    
+    public init(clientId:String = FitpaySDKConfiguration.defaultConfiguration.clientId,
+                redirectUri:String = FitpaySDKConfiguration.defaultConfiguration.redirectUri,
+                userEmail:String?,
+                deviceInfo:DeviceInfo?,
+                hasAccount:Bool = false) {
         self.clientId = clientId
         self.redirectUri = redirectUri
         self.userEmail = userEmail
@@ -25,15 +60,72 @@ open class RtmConfig: NSObject, Mappable {
     }
     
     open func mapping(map: Map) {
-        clientId <- map["clientId"]
-        redirectUri <- map["redirectUri"]
-        userEmail <- map["userEmail"]
-        deviceInfo <- map["paymentDevice"]
-        hasAccount <- map["account"]
-        version <- map["version"]
-        demoMode <- map["demoMode"]
-        customCSSUrl <- map["themeOverrideCssUrl"]
-        demoCardGroup <- map["demoCardGroup"]
-        accessToken <- map["accessToken"]
+        clientId        <- map[RtmConfigDafaultMappingKey.clientId.rawValue]
+        redirectUri     <- map[RtmConfigDafaultMappingKey.redirectUri.rawValue]
+        userEmail       <- map[RtmConfigDafaultMappingKey.userEmail.rawValue]
+        deviceInfo      <- map[RtmConfigDafaultMappingKey.deviceInfo.rawValue]
+        hasAccount      <- map[RtmConfigDafaultMappingKey.hasAccount.rawValue]
+        version         <- map[RtmConfigDafaultMappingKey.version.rawValue]
+        demoMode        <- map[RtmConfigDafaultMappingKey.demoMode.rawValue]
+        customCSSUrl    <- map[RtmConfigDafaultMappingKey.customCSSUrl.rawValue]
+        demoCardGroup   <- map[RtmConfigDafaultMappingKey.demoCardGroup.rawValue]
+        accessToken     <- map[RtmConfigDafaultMappingKey.accessToken.rawValue]
+        language        <- map[RtmConfigDafaultMappingKey.language.rawValue]
+        baseLanguageUrl <- map[RtmConfigDafaultMappingKey.baseLanguageUrl.rawValue]
+    }
+    
+    public func update(value: Any, forKey key: String) {
+        if let mappingKey = RtmConfigDafaultMappingKey(rawValue: key) {
+            switch mappingKey {
+            case .accessToken:
+                accessToken = value as? String
+                break
+            case .clientId:
+                clientId = value as? String
+                break
+            case .redirectUri:
+                redirectUri = value as? String
+                break
+            case .userEmail:
+                userEmail = value as? String
+                break
+            case .deviceInfo:
+                deviceInfo = value as? DeviceInfo
+                break
+            case .hasAccount:
+                hasAccount = value as? Bool ?? false
+                break
+            case .version:
+                version = value as? String
+                break
+            case .demoMode:
+                demoMode = value as? Bool
+                break
+            case .customCSSUrl:
+                customCSSUrl = value as? String
+                break
+            case .demoCardGroup:
+                demoCardGroup = value as? String
+                break
+            case .language:
+                language = value as? String
+            case .baseLanguageUrl:
+                baseLanguageUrl = value as? String
+            }
+        } else {
+            if customs == nil {
+                customs = [:]
+            }
+            
+            customs!.updateValue(value, forKey: key)
+        }
+    }
+    
+    public func jsonDict() -> [String: Any] {
+        var dict = self.toJSON()
+        if let customs = self.customs {
+            dict += customs
+        }
+        return dict
     }
 }
