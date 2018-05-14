09a38b3296b1b7b80367cdeb3a958cc8b8aa4f9c
Jeremiah Harris <jeremiah@fit-pay.com>
2018-05-14 13:33:07 -0600

    Moving cardType to internal because OEMs should not need it.

--- FitpaySDK/A2AVerification/A2AVerificationRequest.swift
+++ FitpaySDK/A2AVerification/A2AVerificationRequest.swift
@@ -3,13 +3,16 @@ import Foundation
 /// Main Object sent back from `verificationFinished`
 @objc public class A2AVerificationRequest: NSObject, Serializable {
     
-    @objc public var cardType: String?
-    
     /// String to build the correct url when returning back from issuer app
     /// This should be saved locally through the process
     @objc public var returnLocation: String?
     
     /// Object containing information needed to pass into the issuer app
     @objc public var context: A2AContext?
+    
+    //MARK: - Internal
+    
+    // Used to disable Mastercard since it is not supported
+    var cardType: String?
 
 }
