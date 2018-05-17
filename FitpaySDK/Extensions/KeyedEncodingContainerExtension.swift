96e20c511eb0d20f329b34e365193f4ba53de997
Jeremiah Harris <jeremiah@fit-pay.com>
2018-05-17 14:47:00 -0600

    duplicate files

--- FitpaySDK/Extensions/KeyedEncodingContainerExtension.swift
+++
@@ -1,9 +0,0 @@
-extension KeyedEncodingContainer {
-
-    mutating func encode<Transformer: EncodingContainerTransformer>(_ value: Transformer.Output?,
-                                                                           forKey key: KeyedEncodingContainer.Key,
-                                                                           transformer: Transformer) throws where Transformer.Input : Encodable {
-        let transformed: Transformer.Input? = transformer.transform(value)
-        try self.encode(transformed, forKey: key)
-    }
-}
