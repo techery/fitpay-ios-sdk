756b0a4aa54e35b29a6f4107b852730e673ce5e3
Jeremiah Harris <jeremiah@fit-pay.com>
2018-06-05 16:17:49 -0600

    Make MockPaymentDeviceConnector more cloneable
    - NSErrorExtension Cleanup

--- FitpaySDK/Extensions/NSErrorExtensions.swift
+++ FitpaySDK/Extensions/NSErrorExtensions.swift
@@ -3,31 +3,32 @@ import Foundation
 import Alamofire
 
 extension NSError {
-    class func error(code: Int, domain: AnyClass, message: String) -> NSError {
-        return NSError(domain: "\(domain.self)", code:code, userInfo: [NSLocalizedDescriptionKey : message])
+    
+    class func error(code: Int, domain: Any, message: String) -> NSError {
+        return NSError(domain: "\(domain.self)", code: code, userInfo: [NSLocalizedDescriptionKey: message])
     }
     
-    class func error(code: RawIntValue & CustomStringConvertible, domain: AnyClass, message: String? = nil) -> NSError {
+    class func error(code: RawIntValue & CustomStringConvertible, domain: Any, message: String? = nil) -> NSError {
         return NSError(domain: "\(domain.self)", code: code.rawValue, userInfo: [NSLocalizedDescriptionKey: message ?? code.description])
     }
 
-    class func error<T: RawIntValue>(code: T, domain: AnyClass, message: String) -> NSError {
+    class func error<T: RawIntValue>(code: T, domain: Any, message: String) -> NSError {
         return NSError(domain: "\(domain.self)", code: code.rawValue, userInfo: [NSLocalizedDescriptionKey: message])
     }
     
-    class func errorWithData(code:Int, domain:AnyClass, data:Data?, alternativeError:NSError? = nil) -> NSError {
+    class func errorWithData(code: Int, domain: Any, data: Data?, alternativeError: NSError? = nil) -> NSError {
         if let messages = data?.errorMessages {
             if messages.count > 0 {
-                return NSError(domain: "\(domain)", code:code, userInfo: [NSLocalizedDescriptionKey : messages[0]])
+                return NSError(domain: "\(domain)", code:code, userInfo: [NSLocalizedDescriptionKey: messages[0]])
             }
         } else if let message = data?.errorMessage {
-            return NSError(domain: "\(domain)", code:code, userInfo: [NSLocalizedDescriptionKey : message])
+            return NSError(domain: "\(domain)", code:code, userInfo: [NSLocalizedDescriptionKey: message])
         } else if let message = data?.UTF8String {
             if message == "" {
-               return NSError(domain: "\(domain)", code:code, userInfo: [NSLocalizedDescriptionKey : "UnexpectedError"])
+               return NSError(domain: "\(domain)", code:code, userInfo: [NSLocalizedDescriptionKey: "UnexpectedError"])
             }
             
-            return NSError(domain: "\(domain)", code:code, userInfo: [NSLocalizedDescriptionKey : message])
+            return NSError(domain: "\(domain)", code:code, userInfo: [NSLocalizedDescriptionKey: message])
         }
                 
         let userInfo:[AnyHashable: Any] = alternativeError?.userInfo != nil ? alternativeError!.userInfo: [NSLocalizedDescriptionKey: "Failed to parse error message"]
@@ -42,19 +43,18 @@ extension NSError {
         return errorWithData(code: response.response?.statusCode ?? 0, domain: domain, data: response.data, alternativeError: response.result.error as NSError?)
     }
 
-    class func unhandledError(_ domain: AnyClass) -> NSError {
+    class func unhandledError(_ domain: Any) -> NSError {
         return NSError(domain:"\(domain)", code:0, userInfo: [NSLocalizedDescriptionKey: "Unhandled error"])
     }
     
-    class func clientUrlError(domain: AnyClass, code: Int, client: RestClient?, url: String?, resource: String) -> NSError? {
-        if let _ = client {
-            if let _ = url {
-                return nil
-            } else {
-                return NSError.error(code: 0, domain: domain, message: "Failed to retrieve url for resource '\(resource)'")
-            }
-        } else {
+    class func clientUrlError(domain: Any, code: Int, client: RestClient?, url: String?, resource: String) -> NSError? {
+        if client == nil {
             return NSError.error(code: 0, domain: domain, message: "\(RestClient.self) is not set.")
+        } else if url == nil {
+            return NSError.error(code: 0, domain: domain, message: "Failed to retrieve url for resource '\(resource)'")
+        } else {
+            return nil
         }
     }
+    
 }
