b96360fcd805c55b2f8f893d61fe2d08ee7d1ddf
Jeremiah Harris <jeremiah@fit-pay.com>
2018-08-05 09:31:57 -0600

    Adding documentation

--- FitpaySDK/Rest/Models/PlatformConfig.swift
+++ FitpaySDK/Rest/Models/PlatformConfig.swift
@@ -1,7 +1,13 @@
 import Foundation
 
+/// Used to be able to enable/disable features remotely from the server
+///
+/// Primarily used for internal SDK purposes
 public struct PlatformConfig: Serializable {
     
+    /// Can be used disable the user event stream from being initialized
+    ///
+    /// server defaults to true
     public let isUserEventStreamsEnabled: Bool
     
     private enum CodingKeys: String, CodingKey {
