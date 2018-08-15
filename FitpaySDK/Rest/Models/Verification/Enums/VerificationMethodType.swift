186a64eb8897b706f7b9951505be221650295178
Jeremiah Harris <jeremiah@fit-pay.com>
2018-08-15 08:29:11 -0600

    Add missing verification types

--- FitpaySDK/Rest/Models/Verification/Enums/VerificationMethodType.swift
+++ FitpaySDK/Rest/Models/Verification/Enums/VerificationMethodType.swift
@@ -1,11 +1,13 @@
 import Foundation
 
 public enum VerificationMethodType: String, Serializable {
-    case textToCardholderNumber             = "TEXT_TO_CARDHOLDER_NUMBER"
-    case emailToCardholderAddress           = "EMAIL_TO_CARDHOLDER_ADDRESS"
-    case cardholderToCallAutomatedNumber    = "CARDHOLDER_TO_CALL_AUTOMATED_NUMBER"
-    case cardholderToCallMannedNumber       = "CARDHOLDER_TO_CALL_MANNED_NUMBER"
-    case cardholderToVisitWebsite           = "CARDHOLDER_TO_VISIT_WEBSITE"
-    case cardholderToUserMobileApp          = "CARDHOLDER_TO_USE_MOBILE_APP"
-    case issuerToCallCardholderNumber       = "ISSUER_TO_CALL_CARDHOLDER_NUMBER"
+    case textToCardholderNumber                 = "TEXT_TO_CARDHOLDER_NUMBER"
+    case emailToCardholderAddress               = "EMAIL_TO_CARDHOLDER_ADDRESS"
+    case cardholderToCallAutomatedNumber        = "CARDHOLDER_TO_CALL_AUTOMATED_NUMBER"
+    case cardholderToCallMannedNumber           = "CARDHOLDER_TO_CALL_MANNED_NUMBER"
+    case cardholderToVisitWebsite               = "CARDHOLDER_TO_VISIT_WEBSITE"
+    case cardholderToCallForAutomatedOTPCode    = "CARDHOLDER_TO_CALL_FOR_AUTOMATED_OTP_CODE"
+    case agentToCallCardholderNumber            = "AGENT_TO_CALL_CARDHOLDER_NUMBER"
+    case cardholderToUserMobileApp              = "CARDHOLDER_TO_USE_MOBILE_APP"
+    case issuerToCallCardholderNumber           = "ISSUER_TO_CALL_CARDHOLDER_NUMBER"
 }
