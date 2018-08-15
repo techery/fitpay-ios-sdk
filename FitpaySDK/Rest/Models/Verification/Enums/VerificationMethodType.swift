caae51d9d9f8e70e07f7e6c9069dfb5e9773d62f
Jeremiah Harris <jeremiah@fit-pay.com>
2018-08-15 08:32:12 -0600

    typo fix

--- FitpaySDK/Rest/Models/Verification/Enums/VerificationMethodType.swift
+++ FitpaySDK/Rest/Models/Verification/Enums/VerificationMethodType.swift
@@ -8,6 +8,6 @@ public enum VerificationMethodType: String, Serializable {
     case cardholderToVisitWebsite               = "CARDHOLDER_TO_VISIT_WEBSITE"
     case cardholderToCallForAutomatedOTPCode    = "CARDHOLDER_TO_CALL_FOR_AUTOMATED_OTP_CODE"
     case agentToCallCardholderNumber            = "AGENT_TO_CALL_CARDHOLDER_NUMBER"
-    case cardholderToUserMobileApp              = "CARDHOLDER_TO_USE_MOBILE_APP"
+    case cardholderToUseMobileApp               = "CARDHOLDER_TO_USE_MOBILE_APP"
     case issuerToCallCardholderNumber           = "ISSUER_TO_CALL_CARDHOLDER_NUMBER"
 }
